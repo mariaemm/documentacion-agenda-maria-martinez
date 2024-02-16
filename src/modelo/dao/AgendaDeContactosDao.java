7af1f68fd9adc2fb1a11de7fe674bc8c256d7782
maria <mariaemartinezmeloni@gmail.com>
2024-02-16 10:22:30 +0100

    docs(agendadecontactosdao): documentado AgendaDeContactosDao

--- src/modelo/dao/AgendaDeContactosDao.java
+++ src/modelo/dao/AgendaDeContactosDao.java
@@ -1,2 +1,19 @@
-package modelo.dao;public interface AgendaDeContactosDao {
+package modelo.dao;
+
+import modelo.javabean.Contacto;
+
+import java.util.ArrayList;
+
+public interface AgendaDeContactosDao {
+
+    // IMPORTAMOS CONTACTO
+    Boolean AltaContacto (Contacto contacto);
+    Boolean EliminarContacto (Contacto contacto);
+    Contacto buscarUno(String nombre);
+    Contacto buscarTelefono(String telefono);
+    Contacto buscarEmail(String email);
+    ArrayList<Contacto> buscarContactosPorTresPrimeros (String nombre);
+    Boolean cambiarDatos (Contacto contacto);
+    ArrayList<Contacto> ContactosPorEmpresa (String empresa);
+
 }
